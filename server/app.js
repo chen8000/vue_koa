c154fe343c4027f915495adbd4a02aa4717fa188
chen <chenzhanghui@yplsec.com>
2020-06-04 19:09:58 +0800

    update

--- server/app.js
+++ server/app.js
@@ -11,16 +11,6 @@ const logger = require('koa-logger')
 const cors = require('koa2-cors')
 const koajwt = require('koa-jwt')
 const { tokenSeccret } = require('./config')
-// const session = require('koa-generic-session')
-// const PgStore = require('koa-pg-session')
-// const dbConfig = require('./db/config')
-
-// 获取数据库配置
-// const {username, password, database, host, port} = dbConfig
-// const { schema } = dbConfig.define
-
-// pg实例
-// const pgStore = new PgStore({ user: username, password, database, host, port },{ schema })
 
 // sequelize
 require('./model/index')
@@ -42,33 +32,6 @@ app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 app.use(cors({ credentials: true }))
-// app.keys = ['keys', 'keykeys']
-// app.use(session({
-//   // secret:'zhanghui666', // 加密session的随机字符串，随便写
-//   // name:'userId', // 设置返回客户端的key 默认是connect.sid   
-//   // resave:false, // 默认为true 表示不管session有没有变化都保存，false表示有变化才保存
-//   // saveUninitialized:true, // 强制把未初始化的session存储 默认设置为true 建议设置成true
-
-//   //cookie的所有参数都可以设置到这里 
-//   cookie:{
-//       // secure:false,
-//       maxAge:50000000  //设置过期时间
-//   }, //secure:true 表示只有在https协议下才可以访问这个cookie
-
-//     rolling:true, // 每次用户刷新也就后重新设置cookie时间，只要用户在过期时间内刷新的页面，
-//                 // cookie就不会过期，过期时间从用户最后一次刷新开始计算
-//   store: pgStore,
-//   // allowEmpty: true
-// }, app))
-
-// 创建session表  一次即可
-// pgStore.setup()
-
-
-// app.use(function *() {
-//   this.body = this.session.counter++;
-// });
-
 
 
 //配置静态模版
